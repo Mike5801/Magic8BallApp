edcba02c9cf73f3d40c94a19c4b0e3b2643ce135
Miguel Weiping Tang Feng <88734115+Mike5801@users.noreply.github.com>
2023-07-21 17:54:44 -0600

    add Ball statefull widget

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(
@@ -20,10 +21,34 @@ class BallPage extends StatelessWidget {
         title: const Text('Ask Me Anything'),
         backgroundColor: Colors.blue[900],
       ),
-      body: Container(
+      body: const Ball(),
+    );
+  }
+}
 
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () => {
+          setState(
+            () {
+              ballNumber = Random().nextInt(4) + 1;
+            },
+          )
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
       ),
-
     );
   }
 }
